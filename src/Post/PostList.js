90107c5f27bd4bc656ef8ffd5729e2821d58842e
Ali Meral <meralmuhammet544@gmail.com>
2023-10-24 16:14:29 +0300

    adding poslist component

--- src/Post/PostList.js
+++ src/Post/PostList.js
@@ -1,7 +1,28 @@
 import React from "react";
+import "./PostList.css";
+import Avatar from "react-avatar";
 
 function PostList() {
-  return <div>PostList</div>;
+  return (
+    <div className="post_list">
+      <Avatar size="40px" name="Wim Mostmans" round="50px" />
+      <div className="postList_info">
+        <div className="postList_header">
+          <h2 className="postList_headerName">
+            mahmut mahmut{" "}
+            <span className="postList_headerUsername">testest21</span>
+          </h2>
+        </div>
+        <div className="postList_content">
+          <p className="postList_text">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam,
+            veritatis.
+          </p>
+        </div>
+        <div className="postList_footer"></div>
+      </div>
+    </div>
+  );
 }
 
 export default PostList;
