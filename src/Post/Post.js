b9b96c4fb217bb453b21d12ed456d012fce91ab8
Ali Meral <meralmuhammet544@gmail.com>
2023-10-25 16:37:21 +0300

    get tweet data in the firebase

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Post.css";
 import PostHeader from "./PostHeader/PostHeader";
 import PostSend from "./PostSend/PostSend";
 import PostList from "./PostList/PostList";
 
+import { db } from "../firebase";
+import { collection, getDocs } from "firebase/firestore";
+
 function Post() {
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    async function getTweet() {
+      const querySnapshot = await getDocs(collection(db, "tweet"));
+      setPost(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          items: doc.data(),
+        }))
+      );
+    }
+    getTweet();
+  }, []);
+
+  console.log(post);
+
   return (
     <div className="post">
       <PostHeader />
       <PostSend />
+      <PostList />
+      <PostList />
+      <PostList />
+      <PostList />
       <PostList />
     </div>
   );
