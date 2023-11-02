4c94afd183454be48d80dc1ca113b5e0df109f0e
Ali Meral <meralmuhammet544@gmail.com>
2023-11-02 16:43:49 +0300

    make searcbox in widget

--- src/Widgets/Widget.js
+++ src/Widgets/Widget.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./Widget.css";
+import { BiSearch } from "react-icons/bi";
 
 function Widget() {
-  return <div>Widget</div>;
+  return (
+    <div className="widget">
+      <div className="widget_header">
+        <div className="widget_search">
+          <BiSearch />
+          <input type="text" placeholder="search" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Widget;
