2ada732b098e8c87550a9deba1a5da94cb910f39
Ali Meral <meralmuhammet544@gmail.com>
2023-11-03 19:21:32 +0300

    add widget footer

--- src/Widgets/Widget.js
+++ src/Widgets/Widget.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./Widget.css";
 import Trend from "./Trend/Trend";
 import { BiSearch, BiTrendingUp } from "react-icons/bi";
+import Avatar from "react-avatar";
 
 import { db } from "../firebase";
 import { collection, getDocs } from "firebase/firestore";
@@ -30,14 +31,40 @@ function Widget() {
           <input type="text" placeholder="search" />
         </div>
       </div>
+
       <div className="widget_trendContent">
         <h2 className="trendContent_title">Things that might interest you</h2>
         {trends.map((trend) => (
           <Trend items={trend.items} key={trend.id} />
         ))}
       </div>
-      <h2>test</h2>
-      <h2>test</h2>
+
+      <div className="widget_footer">
+        <div className="widget_footerContainer">
+          <Avatar size="40px" name="Ahmet Yilmaz" round="50px" />
+          <div className="footer_profileInfo">
+            <h2>Ahmet Yilmaz</h2>
+            <h5>@ahmet333</h5>
+          </div>
+          <button>Follow</button>
+        </div>
+        <div className="widget_footerContainer">
+          <Avatar size="40px" name="Merve Atmaca" round="50px" />
+          <div className="footer_profileInfo">
+            <h2>Merve Atmaca</h2>
+            <h5>@merve2221</h5>
+          </div>
+          <button>Follow</button>
+        </div>
+        <div className="widget_footerContainer">
+          <Avatar size="40px" name="Selim Kaya" round="50px" />
+          <div className="footer_profileInfo">
+            <h2>Selim Kaya</h2>
+            <h5>@selim1111</h5>
+          </div>
+          <button>Follow</button>
+        </div>
+      </div>
     </div>
   );
 }
